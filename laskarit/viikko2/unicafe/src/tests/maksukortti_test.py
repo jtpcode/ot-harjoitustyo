3df50f03e8aa9bb09c76f256693eeddf037c9751
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-03-17 14:23:54 +0200

    Viikko2_tehtävä_6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_lataus_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 20.0)
+
+    def test_rahan_ottaminen_vahentaa_saldoa_jos_rahaa_on_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5.0)
+
+    def test_rahan_ottaminen_ei_muuta_saldoa_jos_rahaa_ei_ole_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_rahan_ottaminen_palauttaa_true_jos_rahaa_on_tarpeeksi(self):      
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+
+    def test_rahan_ottaminen_palauttaa_false_jos_rahaa_ei_ole_tarpeeksi(self):      
+        self.assertEqual(self.maksukortti.ota_rahaa(1500), False)
