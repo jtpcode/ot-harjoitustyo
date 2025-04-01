46cd808f6a9e2a13657fdd1e53f7b5f8460bbf73
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-04-01 10:43:08 +0300

    Tweak tests

--- src/tests/repositories/user_repository_test.py
+++ src/tests/repositories/user_repository_test.py
@@ -9,13 +9,19 @@ class TestUserRepository(unittest.TestCase):
         initialize_database.initialize_database()
         self.user_alfa = User('alfa', '1234alfa5678')
         self.user_beta = User('beta', '1234beta5678')
-
-    def test_create(self):
         user_repository.create(self.user_alfa)
         user_repository.create(self.user_beta)
+
+    def test_create(self):
+        users = user_repository.find_all()
+
+        self.assertEqual(users[0].username, self.user_alfa.username)
+
+    def test_find_all(self):
         users = user_repository.find_all()
 
         self.assertEqual(len(users), 2)
         self.assertEqual(users[0].username, self.user_alfa.username)
         self.assertEqual(users[1].username, self.user_beta.username)
+        
     
